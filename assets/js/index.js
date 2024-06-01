3e80e9ac3e1f7f1fa9f2adf372b935be8e5e486f
Arthur Souto <arthursantostavarescontatos@gmail.com>
2024-06-01 16:57:15 -0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+
+
+function menu() {
+
+    
+   
+const x = document.querySelector("nav");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      
+}

6bc8de50e3fd98590a1422013e0a3e3b43673e19
Arthur Souto <arthursantostavarescontatos@gmail.com>
2024-06-04 09:42:03 -0300

    ajietando a bagunça pt 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,13 +2,33 @@
 
 function menu() {
 
-    
-   
-const x = document.querySelector("nav");
-        if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-      
-}
+    const x = document.querySelector("nav");
+            if (x.style.display === "none") {
+              x.style.display = "block";
+            } else {
+              x.style.display = "none";
+            }
+          
+    }
+
+
+    const myObserver = new IntersectionObserver( (entries) => {
+      entries.forEach( (entry) => {
+          if(entry.isIntersecting  = true)  {
+              entry.target.classList.add('show')
+          }
+  
+          else {
+              entry.target.classList.remove('show')
+          }
+      })
+  })
+  
+  const webAnimation = document.querySelectorAll(".animation-web") 
+  
+  
+  webAnimation.forEach(element => {
+      myObserver.observe(element)
+  });
+
+
